155f9cf3977912c4ce5f5a7174dbfa67b7367e75
Mohisn Bhat <mohsinsmsn@gmail.com>
2025-05-05 18:32:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,7 @@
 sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl wget screen git lsof
 
 #install ufw
-sudo apt install ufw -y
-sudo ufw allow 22
-sudo ufw allow 3000/tcp
-sudo ufw enable
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb <<< $'Y
-sudo dpkg -i cloudflared-linux-amd64.deb
+
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt update && sudo apt install -y nodejs
